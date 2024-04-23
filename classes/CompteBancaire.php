8e9a5ed21394d1db44d636aa4cf2f92f15b9f101
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-04-23 20:16:02 +0200

    ajout indentation

--- classes/CompteBancaire.php
+++ classes/CompteBancaire.php
@@ -2,26 +2,28 @@
 
 
 // Création d'une classe CompteBancaire
-class CompteBancaire {
+class CompteBancaire
+{
     // Propriétés privées de la classe
     private string $libelle;
     private float $solde;
     private string $devise;
     private Titulaire $titulaire;
-    
+
 
     // Construction de la classe CompteBancaire, il est appelé à chaque création d'un nouvel objet CompteBancaire
-    public function __construct(string $libelle, float $solde, string $devise, Titulaire $titulaire) {
-    // Initialisation des propriétés avec les valeurs fournies lors de la création de l'objet
-    $this->libelle = $libelle;
-    $this->solde = $solde;
-    $this->devise = $devise;
-    $this->titulaire = $titulaire;
+    public function __construct(string $libelle, float $solde, string $devise, Titulaire $titulaire)
+    {
+        // Initialisation des propriétés avec les valeurs fournies lors de la création de l'objet
+        $this->libelle = $libelle;
+        $this->solde = $solde;
+        $this->devise = $devise;
+        $this->titulaire = $titulaire;
 
-    // Ici, l'objet CompteBancaire s'ajoute automatiquement au titulaire passé en paramètre
-    $this->titulaire->ajouterCompte($this);
+        // Ici, l'objet CompteBancaire s'ajoute automatiquement au titulaire passé en paramètre
+        $this->titulaire->ajouterCompte($this);
     }
-    
+
     // Création des getters/setters
     public function getLibelle()
     {
@@ -31,19 +33,19 @@ class CompteBancaire {
     public function setLibelle($libelle)
     {
         $this->libelle = $libelle;
-        
+
         return $this;
     }
-    
+
     public function getSolde()
     {
         return $this->solde;
     }
-    
+
     public function setSolde($solde)
     {
         $this->solde = $solde;
-        
+
         return $this;
     }
 
@@ -51,23 +53,25 @@ class CompteBancaire {
     {
         return $this->devise;
     }
-    
+
     public function setDevise($devise)
     {
         $this->devise = $devise;
-        
+
         return $this;
     }
 
     // Création methode pour faire un dépôt d'argent
-    public function depot(float $montant) {
+    public function depot(float $montant)
+    {
         $this->solde += $montant;
         return $this->solde;
     }
 
     // Création methode pour retirer de l'argent
-    public function retrait(float $montant) {
-        if($montant <= $this->solde) {
+    public function retrait(float $montant)
+    {
+        if ($montant <= $this->solde) {
             $this->solde -= $montant;
             return $this->solde;
         } else {
@@ -76,9 +80,10 @@ class CompteBancaire {
     }
 
     // Création methode virement
-    public function virement(CompteBancaire $compteDestinataire, float $montant) {
+    public function virement(CompteBancaire $compteDestinataire, float $montant)
+    {
         // Vérifier si le solde permet le virement
-        if($this->solde >= $montant) {
+        if ($this->solde >= $montant) {
             // Soustraire le montant du solde actuel
             $this->solde -= $montant;
             // Ajouter le montant au compte destinataire
@@ -90,9 +95,8 @@ class CompteBancaire {
     }
 
     // Création d'une méthode toString pour récuperer les informations d'un compte bancaire, notamment nom et prénom
-    public function __toString() {
+    public function __toString()
+    {
         return "Libellé : " . $this->libelle . ". Solde : " . $this->solde . " " . $this->devise . "<br>";
     }
 }
-
-    
--- classes/Titulaire.php
+++ classes/Titulaire.php
@@ -1,7 +1,8 @@
 <?php
 
 // Création d'une classe Titulaire
-class Titulaire {
+class Titulaire
+{
     // Propriétés privées de la classe
     private string $nom;
     private string $prenom;
@@ -11,14 +12,15 @@ class Titulaire {
 
 
     // Constructeur de la classe Titulaire, il est appelé à chaque création d'un nouvel objet Titulaire
-    public function __construct(string $nom, string $prenom, string $dateNaissance, string $ville) {
-    // Initialisation des propriétés avec les valeurs fournies lors de la création de l'objet
-    $this->nom = $nom;
-    $this->prenom = $prenom;
-    $this->dateNaissance = new DateTime($dateNaissance);
-    $this->ville = $ville;
-    $this->comptes = []; // Initialisation du tableau de comptes en tant que tableau vide
-    
+    public function __construct(string $nom, string $prenom, string $dateNaissance, string $ville)
+    {
+        // Initialisation des propriétés avec les valeurs fournies lors de la création de l'objet
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->dateNaissance = new DateTime($dateNaissance);
+        $this->ville = $ville;
+        $this->comptes = []; // Initialisation du tableau de comptes en tant que tableau vide
+
     }
 
     // Création des getters/setters
@@ -47,7 +49,7 @@ class Titulaire {
     }
 
     public function getDateNaissance()
-    {   
+    {
         return $this->dateNaissance->format("d/m/Y");
     }
 
@@ -83,22 +85,25 @@ class Titulaire {
     }
 
     // Création d'une méthode pour calculer l'âge
-    public function getAge() {
+    public function getAge()
+    {
         $dateActuelle = new DateTime();
         $age = date_diff($dateActuelle, $this->dateNaissance);
         return $age->y;
     }
 
     // Création méthode pour ajouter un compte au titulaire
-    public function ajouterCompte(CompteBancaire $compte) {
+    public function ajouterCompte(CompteBancaire $compte)
+    {
         // Ajout du compte à la liste des comptes
         $this->comptes[] = $compte;
     }
-    
+
     // Création d'une méthode toString pour récuperer toutes les informations d'un titulaire (dont l'âge) et l'ensemble des comptes apparenant à celui-ci
-    public function __toString() {
+    public function __toString()
+    {
         $info = $this->nom . " " . $this->prenom . " " . $this->getAge() . " ans<br>";
-        foreach($this->comptes as $compte) {
+        foreach ($this->comptes as $compte) {
             $info .= $compte->__toString();
         }
         return $info;
